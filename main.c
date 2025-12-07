a64ccccbb1476e06bdac0ad1df12aaa61b57e7c9
Kheldin <kheldin.acherchour@gmail.com>
2025-12-07 22:16:41 +0100

    On avance

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	Setup_WindowRenderer(SDL_Window **window, SDL_Renderer **renderer, SDL_RendererInfo *renderer_info)
+int	setup_WindowRenderer(SDL_Window **window, SDL_Renderer **renderer, SDL_RendererInfo *renderer_info)
 {
 	*window = SDL_CreateWindow("Ramdom Walk", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 								640, 480, SDL_WINDOW_SHOWN);
@@ -26,6 +26,7 @@ int	Setup_WindowRenderer(SDL_Window **window, SDL_Renderer **renderer, SDL_Rende
 	{
 		fprintf(stderr, "Getting renderer's info failed : %s\n", SDL_GetError());
 		SDL_DestroyWindow(*window);
+		SDL_DestroyRenderer(*renderer);
 		SDL_Quit();
 		return (-1);
 	}
@@ -35,6 +36,30 @@ int	Setup_WindowRenderer(SDL_Window **window, SDL_Renderer **renderer, SDL_Rende
 		printf("Renderer is using software rendering\n");
 	return (0);
 }
+
+void	display_rectangles(SDL_Window *window, SDL_Renderer *renderer)
+{
+	SDL_Rect rect = {100, 100, 50, 50};
+
+	SDL_SetRenderDrawColor(renderer, 255, 127, 40, 255);
+	SDL_RenderFillRect(renderer, &rect);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(500);
+	rect.x = 50;
+	rect.y = 50;
+	SDL_RenderFillRect(renderer, &rect);
+	SDL_RenderPresent(renderer);
+}
+
+int	set_window_color(SDL_Renderer *renderer, SDL_Color color)
+{
+	if (SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a) < 0)
+		return (-1);
+	if (SDL_RenderClear(renderer) < 0)
+		return (-1);
+	return (0);
+}
+// Ajouter le delay puis changet le fond avec set window color
 int main (void)
 {
     SDL_Window			*window = NULL;
@@ -48,10 +73,13 @@ int main (void)
 		SDL_Quit();
 		return (EXIT_FAILURE);
 	}
-	if (Setup_WindowRenderer(&window, &renderer, &renderer_info) != 0)
+	// Setup the window and the renderer
+	if (setup_WindowRenderer(&window, &renderer, &renderer_info) != 0)
 		return (EXIT_FAILURE);
+	display_rectangles(window, renderer);
 	SDL_Delay(3000);
 	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
 	SDL_Quit();
 	return (EXIT_SUCCESS);
 }
